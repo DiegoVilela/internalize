b1f4e915c5c514d4a29d764e1f915d36d6322ee8
Diego Vilela <vilelaphp@gmail.com>
2021-01-25 16:52:55 -0300

    configure the admin area

--- cis/admin.py
+++ cis/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import (
+    Client,
+    Site,
+    ISP,
+    Manufacturer,
+    Circuit,
+    Contract,
+    Appliance,
+    Setup,
+    Credential,
+    CI
+)
 
-# Register your models here.
+admin.site.register(Client)
+admin.site.register(Site)
+admin.site.register(ISP)
+admin.site.register(Manufacturer)
+admin.site.register(Circuit)
+admin.site.register(Contract)
+admin.site.register(Appliance)
+admin.site.register(Setup)
+admin.site.register(Credential)
+admin.site.register(CI)
