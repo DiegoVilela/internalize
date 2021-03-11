36537df4e7d027992e90d023d0b3a6ab1a9c0f29
Diego Vilela <vilelaphp@gmail.com>
2021-03-11 15:03:00 -0300

    BASE_DIR is a instance of Path()

--- internalize/settings.py
+++ internalize/settings.py
@@ -60,7 +60,7 @@ ROOT_URLCONF = 'internalize.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [Path(BASE_DIR / 'templates')],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
