3b4d64800385ad23bbb887d1146be5bff040942a
Diego Vilela <vilelaphp@gmail.com>
2021-03-15 21:34:35 -0300

    refactor tests, it was good as an exercise but will be difficult to maintain

--- cis/tests/tests_views.py
+++ cis/tests/tests_views.py
@@ -1,12 +1,69 @@
+from datetime import timedelta
+from django.utils import timezone
 from django.test import TestCase
 from django.shortcuts import reverse
 
-from cis.models import Client, Site, User, Appliance, Manufacturer
+from cis.models import Client, Site, User, Appliance, Manufacturer, CI, Contract
 
 
-class SiteAndApplianceViewTest(TestCase):
+class TestDetail:
     """
-    Test both Site and Appliance views.
+    Wraps the details of each test.
+
+    Applied to Site, Appliance, and CI
+    """
+    def __init__(self, message, context_object_name, template_name, lookup_text):
+        self.letter = None
+        self.message = message
+        self.context_object_name = context_object_name
+        self.template_name = template_name
+        self.lookup_text = lookup_text
+
+    @property
+    def contains(self):
+        return f'{self.lookup_text}{self.letter}'
+
+    @property
+    def not_contains(self):
+        return f'{self.lookup_text}{self.get_other_client()}'
+
+    def get_other_client(self):
+        """
+        Return the other client based on the current one.
+        """
+        client = 'A'
+        if self.letter == 'A':
+            client = 'B'
+        return client
+
+
+# Maps the details of tests applied to
+# Site, Appliance, and, CI, respectively.
+MAP_URLS_TO_TESTES = {
+    reverse('cis:manage_client_sites'): TestDetail(
+        message='No site was found.',
+        context_object_name='formset',
+        template_name='cis/manage_client_sites.html',
+        lookup_text='Site Client ',
+    ),
+    reverse('cis:appliance_list'): TestDetail(
+        message='No appliance was found.',
+        context_object_name='appliance_list',
+        template_name='cis/appliance_list.html',
+        lookup_text='SERIAL_CLIENT_',
+    ),
+    reverse('cis:ci_list', args=(0,)): TestDetail(
+        message='No configuration item was found.',
+        context_object_name='ci_list',
+        template_name='cis/ci_list.html',
+        lookup_text='HOST_',
+    ),
+}
+
+
+class SiteApplianceAndCIViewTest(TestCase):
+    """
+    Test CI, Site and Appliance views.
     """
     users = {}
 
@@ -15,100 +72,68 @@ class SiteAndApplianceViewTest(TestCase):
         """
         Set up the database to be used in all testes in this class.
 
-        Client A (plus user_a, Site Client A, and Appliance SERIAL_CLIENT_A)
-        Client B (plus user_b, Site Client B, and Appliance SERIAL_CLIENT_B)
+        Client A and Client B will have their respective User, CI, and Site.
         cls.users = {'a': user_a, 'b': user_b}
         """
 
         manufacturer = Manufacturer.objects.create(name='Cisco')
-        for letter in ['a', 'b']:
-            client = Client.objects.create(name=f'Client {letter.upper()}')
-            client.site_set.create(client=client, name=f'Site Client {letter.upper()}')
-            Appliance.objects.create(
-                client=client,
-                serial_number=f'SERIAL_CLIENT_{letter.upper()}',
-                manufacturer=manufacturer,
-                model='ABC123',
-                virtual=True,
-            )
+        contract = create_contract()
+        for letter in ['A', 'B']:
+            client = Client.objects.create(name=f'Client {letter}')
+            site = Site.objects.create(client=client, name=f'Site Client {letter}')
+            create_appliance(client, manufacturer, letter)
+            create_ci(client, site, letter, contract)
             user = User.objects.create_user(f'user_{letter}', password='faith', client=client)
             cls.users.update({letter: user})
             cls.manufacturer = manufacturer
 
     def test_show_correct_items_by_client(self):
 
-        def other_client(current_client):
-            """Return the other client based on the current one"""
-            client = 'A'
-            if current_client == 'a':
-                client = 'B'
-            return client
-
-        # for client 'a' and client 'b'
+        # for client 'A' and client 'B'
         for k, user in self.users.items():
-
-            # login the current user
             self.client.force_login(user)
 
-            # test Site
-            response = self.client.get(reverse('cis:manage_client_sites'))
-            self.assertEqual(response.status_code, 200)
-            self.assertTemplateUsed(response, 'cis/manage_client_sites.html')
-            self.assertEqual(len(response.context['formset']), 1)
-            self.assertContains(response, f'Site Client {k.upper()}', count=1)
-            self.assertNotContains(response, f'Site Client {other_client(k)}')
+            # test Site, Appliance, and CI
+            for url, test in MAP_URLS_TO_TESTES.items():
+                test.letter = k
+                response = self.client.get(url)
+                self.assertEqual(response.status_code, 200)
+                self.assertTemplateUsed(response, test.template_name)
+                self.assertEqual(len(response.context[test.context_object_name]), 1)
+                self.assertContains(response, test.contains, count=1)
+                self.assertNotContains(response, test.not_contains)
 
-            # test Appliance
-            response = self.client.get(reverse('cis:appliance_list'))
-            self.assertEqual(response.status_code, 200)
-            self.assertTemplateUsed(response, 'cis/appliance_list.html')
-            self.assertEqual(len(response.context['appliance_list']), 1)
-            self.assertContains(response, f'SERIAL_CLIENT_{k.upper()}', count=1)
-            self.assertNotContains(response, f'SERIAL_CLIENT_{other_client(k)}')
-
-            # logout the current user
-            response.client.logout()
+            self.client.logout()
 
     def test_raise_exception_on_unapproved_user(self):
-        user = self.users['a']
+        user = self.users['A']
         user.client = None
         user.save()
         self.client.force_login(user)
 
-        # test Site
-        response = self.client.get(reverse('cis:manage_client_sites'))
-        self.assertIsNone(response.context)
-        self.assertEqual(response.status_code, 403)
+        # test Site, Appliance, and CI
+        for url in MAP_URLS_TO_TESTES.keys():
+            response = self.client.get(url)
+            self.assertIsNone(response.context)
+            self.assertEqual(response.status_code, 403)
 
-        # test Appliance
-        response = self.client.get(reverse('cis:appliance_list'))
-        self.assertIsNone(response.context)
-        self.assertEqual(response.status_code, 403)
-
-        user.client = self.users['a'].client
+        user.client = self.users['A'].client
         user.save()
 
-    def test_no_found(self):
+    def test_not_found(self):
         client = Client.objects.create(name=f'Client C')
         user = User.objects.create_user(f'user_c', password='faith', client=client)
         self.client.force_login(user)
 
-        # test Site
-        response = self.client.get(reverse('cis:manage_client_sites'))
-        self.assertContains(response, 'No site was found.', count=1)
-        self.assertEqual(len(response.context['formset']), 0)
-        self.assertNotContains(response, 'Site Client A')
-        self.assertNotContains(response, 'Site Client B')
-
-        # test Appliance
-        response = self.client.get(reverse('cis:appliance_list'))
-        self.assertContains(response, 'No appliance was found.')
-        self.assertEqual(len(response.context['appliance_list']), 0)
-        self.assertNotContains(response, 'SERIAL_CLIENT_A')
-        self.assertNotContains(response, 'SERIAL_CLIENT_B')
+        # test Site, Appliance, and CI
+        for url, test in MAP_URLS_TO_TESTES.items():
+            response = self.client.get(url)
+            self.assertContains(response, test.message, count=1)
+            self.assertEqual(len(response.context[test.context_object_name]), 0)
+            self.assertNotContains(response, test.not_contains)
 
     def test_create(self):
-        self.client.force_login(self.users['a'])
+        self.client.force_login(self.users['A'])
 
         # test Site
         data = {'name': "Paulista Avenue"}
@@ -128,3 +153,34 @@ class SiteAndApplianceViewTest(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'cis/appliance_form.html')
         self.assertContains(response, 'SERIAL_1', count=1)
+
+
+def create_appliance(client, manufacturer, letter):
+    Appliance.objects.create(
+        client=client,
+        serial_number=f'SERIAL_CLIENT_{letter}',
+        manufacturer=manufacturer,
+        model='ABC123',
+        virtual=True,
+    )
+
+
+def create_contract():
+    return Contract.objects.create(
+        name='CONTRACT',
+        begin=timezone.now(),
+        end=timezone.now() + timedelta(days=356),
+        description='Contract Description',
+    )
+
+
+def create_ci(client, site, letter, contract):
+    CI.objects.create(
+        client=client,
+        site=site,
+        hostname=f'HOST_{letter}',
+        ip='10.10.20.20',
+        description=f'Configuration Item {letter}',
+        deployed=True,
+        contract=contract,
+    )
