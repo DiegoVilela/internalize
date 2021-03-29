05617af0e0657b61e5211b025b8f2106a95569c0
Diego Vilela <vilelaphp@gmail.com>
2021-03-29 15:57:09 -0300

    test CI model

--- cis/tests/tests_models.py
+++ cis/tests/tests_models.py
@@ -2,7 +2,7 @@ from django.shortcuts import reverse
 from django.test import TestCase
 from django.db.utils import IntegrityError
 
-from ..models import User, Client, Site, Manufacturer, Contract, Appliance
+from ..models import User, Client, Site, Manufacturer, Contract, Appliance, CI
 
 
 class UserTest(TestCase):
@@ -143,10 +143,71 @@ class ApplianceTest(TestCase):
         )
 
 
+class CITest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.company_client = Client.objects.create(name='New Client')
+        cls.ci = _create_ci(cls.company_client)
+
+    def test_as_string_returns_full_name(self):
+        self.assertEqual(
+            str(self.ci),
+            f'{self.ci.client.name} | {self.ci.site.name} | {self.ci.hostname} | {self.ci.ip}'
+        )
+
+    def test_duplicate_hostname_by_client_raises_exception(self):
+        with self.assertRaises(IntegrityError):
+            _create_ci(self.company_client)
+
+    def test_duplicate_hostname_different_client_is_ok(self):
+        client = Client.objects.create(name='Different Client')
+        self.assertIsNotNone(_create_ci(client))
+
+    def test_object_is_of_site_type(self):
+        self.assertIsInstance(self.ci, CI)
+
+    def test_absolute_url_returns_correct_url(self):
+        self.assertEqual(
+            self.ci.get_absolute_url(),
+            reverse('cis:ci_detail', args=[self.ci.pk])
+        )
+
+    def test_appliances_many_to_many(self):
+        manufacturer = Manufacturer.objects.create(name='Cisco')
+        appliances = set()
+        for i in range(1, 3):
+            appliances.add(Appliance.objects.create(
+                client=self.company_client,
+                serial_number=f'SERIAL_{i}',
+                manufacturer=manufacturer,
+                model='ABC123'
+            ))
+        self.ci.appliances.set(appliances)
+        self.assertEqual(len(self.ci.appliances.all()), 2)
+
+
 def _create_appliance(client, serial, manufacturer, model):
     return Appliance.objects.create(
         client=client,
         serial_number=serial,
         manufacturer=manufacturer,
         model=model,
+    )
+
+
+def _create_ci(client: Client) -> CI:
+    site = Site.objects.create(name='New Site', client=client)
+    contract = Contract.objects.create(
+        name='CT-001',
+        begin='2021-01-01',
+        end='2022-01-01',
+        description='Contract description.',
+    )
+    return CI.objects.create(
+        client=client,
+        site=site,
+        hostname='NEW_HOST',
+        ip='10.10.20.20',
+        description='The description.',
+        contract=contract,
     )
