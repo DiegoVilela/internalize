5f83876c62fd513a5616243e093873b33bb27e21
Diego Vilela <vilelaphp@gmail.com>
2021-04-05 22:07:34 -0300

    test Appliance

--- cis/tests/tests_functionals.py
+++ cis/tests/tests_functionals.py
@@ -1,10 +1,17 @@
+"""
+Functional tests.
+
+Requires Selenium and geckodriver.
+"""
+
 from django.conf import settings
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.firefox.webdriver import WebDriver
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.support.select import Select
 from selenium.webdriver.support.wait import WebDriverWait
+from selenium.common.exceptions import NoSuchElementException
 
 from ..models import User, Client, Site
 from ..urls import app_name
@@ -14,17 +21,54 @@ PASSWORD = '123'
 SESSION_COOKIE = settings.SESSION_COOKIE_NAME
 
 
-class LoginTest(StaticLiveServerTestCase):
+class CommonTestMixin:
+    """
+    Add common fixtures and methods to TestCases.
+
+    A user is logged in before each test.
+
+    Place the mixin early in the MRO in order to isolate
+    setUpClass()/tearDownClass().
+    """
+    fixtures = ['data_functional.json']
 
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
         cls.driver = WebDriver()
 
+    def setUp(self):
+        # Login via Django
+        user = User.objects.get(pk=1)
+        self.client.force_login(user)
+        cookie = self.client.cookies[SESSION_COOKIE]
+
+        # Selenium will use the current domain to set the cookie
+        self.driver.get(f'{self.live_server_url}/any-404')
+        self.driver.add_cookie({
+            'name': SESSION_COOKIE,
+            'value': cookie.value,
+            'secure': False,
+        })
+
     @classmethod
     def tearDownClass(cls):
         cls.driver.quit()
         super().tearDownClass()
+
+    @staticmethod
+    def _get_alert_success_text(driver: WebDriver) -> str:
+        msg = WebDriverWait(driver, 2).until(
+            lambda d: d.find_element(By.CSS_SELECTOR, '.alert-success')
+        )
+        return msg.text
+
+
+class LoginTest(CommonTestMixin, StaticLiveServerTestCase):
+    fixtures = None
+
+    def setUp(self):
+        pass
 
     def test_login_unapproved_user_shows_alert(self):
         User.objects.create_user(
@@ -66,32 +110,8 @@ class LoginTest(StaticLiveServerTestCase):
         self.driver.find_element(By.LINK_TEXT, 'Logout').click()
 
 
-class SiteTest(StaticLiveServerTestCase):
-    fixtures = ['data_functional.json']
-
-    @classmethod
-    def setUpClass(cls):
-        super().setUpClass()
-        cls.driver = WebDriver()
-
-    def setUp(self):
-        # Login via Django
-        user = User.objects.get(pk=1)
-        self.client.force_login(user)
-        cookie = self.client.cookies[SESSION_COOKIE]
-
-        # Selenium will use the current domain to set the cookie
-        self.driver.get(f'{self.live_server_url}/any-404')
-        self.driver.add_cookie({
-            'name': SESSION_COOKIE,
-            'value': cookie.value,
-            'secure': False,
-        })
-
-    @classmethod
-    def tearDownClass(cls):
-        cls.driver.quit()
-        super().tearDownClass()
+class SiteTest(CommonTestMixin, StaticLiveServerTestCase):
+    """Test all features related to the Site model."""
 
     def test_create_site(self):
         self.driver.get(f'{self.live_server_url}/{app_name}/site/create/')
@@ -125,9 +145,38 @@ class SiteTest(StaticLiveServerTestCase):
         msg = self._get_alert_success_text(self.driver)
         self.assertTrue('The sites were updated successfully.' in msg)
 
-    @staticmethod
-    def _get_alert_success_text(driver: WebDriver) -> str:
-        msg = WebDriverWait(driver, 2).until(
-            lambda d: d.find_element(By.CSS_SELECTOR, '.alert-success')
-        )
-        return msg.text
+
+class ApplianceTest(CommonTestMixin, StaticLiveServerTestCase):
+    def test_create_appliance(self):
+        self.driver.get(f'{self.live_server_url}/{app_name}/appliance/create/')
+        h1 = self.driver.find_element(By.TAG_NAME, 'h1')
+        self.assertEqual(h1.text, 'Appliance')
+
+        the_serial_number = 'DEF456'
+        serial = self.driver.find_element(By.ID, 'id_serial_number')
+        serial.send_keys(the_serial_number)
+
+        manufacturer = self.driver.find_element(By.ID, 'id_manufacturer')
+        manufacturer_select = Select(manufacturer)
+        manufacturer_select.select_by_index(1)
+
+        model = self.driver.find_element(By.ID, 'id_model')
+        model.send_keys('C9000')
+
+        serial = self.driver.find_element(By.ID, 'id_serial_number')
+        self.assertEqual(serial.get_attribute('value'), the_serial_number)
+
+    def test_viewing_appliance(self):
+        _id = 3
+        self.driver.get(f'{self.live_server_url}/{app_name}/appliance/{_id}')
+        serial = self.driver.find_element(By.ID, 'id_serial_number')
+        self.assertEqual(serial.get_attribute('value'), f'ABC12{_id}')
+
+    def test_listing_appliances(self):
+        self.driver.get(f'{self.live_server_url}/{app_name}/appliances/')
+        h1 = self.driver.find_element(By.TAG_NAME, 'h1')
+        self.assertEqual(h1.text, 'Appliances')
+
+        serials = self.driver.find_elements(By.CSS_SELECTOR, 'td>a')
+        for i, serial in enumerate(serials):
+            self.assertEqual(serial.text, f'ABC12{i + 1}')
