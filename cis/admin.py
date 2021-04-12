54d37773a8ef8dbf968ea4b0e41f5c2945cf372b
Diego Vilela <vilelaphp@gmail.com>
2021-04-12 15:06:26 -0300

    improve admin listing

--- cis/admin.py
+++ cis/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import reverse
 from django.utils.html import format_html
+from django.utils.safestring import mark_safe
 
 from .models import (
     Client, Place, ISP, Circuit,
@@ -10,12 +11,32 @@ from .models import (
 
 @admin.register(Client)
 class ClientAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name',)
+    list_display = ('name', 'view_places')
+    search_fields = ('name', 'place__name')
+
+    @admin.display(description='Places')
+    def view_places(self, obj):
+        places = obj.place_set.all()
+        places_link_list = ['<ul>']
+        for place in places:
+            url = f'{reverse("admin:cis_place_change", args={place.pk})}'
+            safe_link = format_html('<a href="{}">{}</a>', url, place.name)
+            places_link_list.append(f'<li>{safe_link}</li>')
+        places_link_list.append('</ul>')
+        return mark_safe('\n'.join(places_link_list))
 
 
 @admin.register(Place)
 class PlaceAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'client')
+    list_display = ('name', 'client_link', 'description')
+    list_filter = ('client',)
+    list_editable = ('description',)
+    search_fields = ('name', 'client__name', 'description')
+
+    @admin.display(description='Client', ordering='client__name')
+    def client_link(self, obj):
+        url = f'{reverse("admin:cis_client_change", args={obj.client.pk})}'
+        return format_html('<a href="{}">{}</a>', url, obj.client.name)
 
 
 @admin.register(Appliance)
@@ -28,6 +49,8 @@ class ApplianceAdmin(admin.ModelAdmin):
         'virtual',
     )
     list_filter = ('client', 'manufacturer', 'virtual')
+    list_editable = ('client', 'manufacturer', 'model', 'virtual')
+    search_fields = ('serial_number', 'model', 'client__name', 'manufacturer__name')
 
 
 @admin.register(Manufacturer)
@@ -39,8 +62,9 @@ class ManufacturerAdmin(admin.ModelAdmin):
 @admin.register(Contract)
 class ContractAdmin(admin.ModelAdmin):
     list_display = ('name', 'begin', 'end', 'description')
+    list_editable = ('begin', 'end', 'description')
     list_filter = ('begin', 'end')
-    search_fields = ('name',)
+    search_fields = ('name', 'description', 'begin', 'end')
 
 
 @admin.action(description='Mark selected CIs as approved')
@@ -56,17 +80,41 @@ class CIAdmin(admin.ModelAdmin):
         'place',
         'ip',
         'description',
-        'view_appliances',
         'deployed',
         'business_impact',
-        'status',
         'contract',
-
+        'status',
+        'view_appliances',
     )
     list_filter = ('status', 'client__name', 'place', 'deployed', 'contract')
-    readonly_fields = ('status',)
     actions = [approve_selected_cis]
+    readonly_fields = ('status',)
+    fieldsets = (
+        ('Client', {'fields': ((), ('client', 'place',))}),
+        ('Configuration Item', {'fields': (
+            'appliances',
+            ('hostname', 'ip', 'deployed'),
+            ('description', 'business_impact'),
+        )}),
+        ('Contract', {'fields': ('contract',)}),
+        ('Credentials', {
+            'fields': (
+                (),
+                ('username', 'password', 'enable_password', 'instructions')),
+            'classes': ('collapse',),
+        }),
+    )
+    filter_horizontal = ('appliances',)
+    list_editable = (
+        'place',
+        'ip',
+        'description',
+        'deployed',
+        'business_impact',
+        'contract'
+    )
 
+    @admin.display(description='Appliances')
     def view_appliances(self, obj):
         count = obj.appliances.count()
         url = f'{reverse("admin:cis_appliance_changelist")}?ci__exact={obj.pk}'
